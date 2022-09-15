646172b662298c931024e97140f495d59ecfc071
Teresa Ribeiro <tmv.ribeiro@campus.fct.unl.pt>
2022-09-15 22:35:39 +0100

    Added Dog and Cat.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@
  * Demo for the Git lab on Software Engineering
  */
 public class Main {
-    public static void main (String[] args){
-        System.out.println("Super Animal git demo");
+    public static void main(String[] args) {
+        System.out.println("Let's hear it from the pets");
+        Animal dog = new Dog("Bobby");
+        Animal cat = new Cat("Kitty");
+        System.out.println(dog.getName() + " says " + dog.speak());
+        System.out.println(cat.getName() + " says " + cat.speak());
     }
 }
