1eff17936240274b5da9ad2def9dc8a52a85ad73
Teresa <tmv.ribeiro@campus.fct.unl.pt>
2022-09-15 17:33:50 +0100

    Initial version of our main class.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author Teresa Ribeiro 60756
+ * Demo for the Git lab on Software Engineering
+ */
+public class Main {
+    public static void main (String[] args){
+        System.out.println("Animal git demo");
+    }
+}
